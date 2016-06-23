957f6faae934a29db64f27892a2881e3cc07d2b7
mhrezaei <mr.mhrezaei@gmail.com>
2016-06-23 23:25:06 +0430

    hadi force change password and reset password

---
+++ app/Http/Requests/OldPasswordRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+use App\Providers\ValidationServiceProvider;
+
+class OldPasswordRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        //$input = $this->all();
+        return [
+            'password' => 'required|min:8', //no need to check existence here.
+            'password2' => 'required|same:password',
+        ];
+    }
+
+    public function all()
+    {
+        $value	= parent::all();
+        $purified = ValidationServiceProvider::purifier($value,[
+            'password'  =>  'ed',
+            'password2'  =>  'ed',
+        ]);
+        return $purified;
+
+    }
+}
