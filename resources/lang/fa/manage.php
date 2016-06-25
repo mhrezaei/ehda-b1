0ebe53d453f24544fe8b63074e66c2905cec7ffe
chieftaha <chieftaha@gmail.com>
2016-06-25 23:18:17 +0430

    Admin panel (#2)
    * taha admin-panel general appearance
    
    * taha user roles
    
    * taha roles

--- resources/lang/fa/manage.php
+++ resources/lang/fa/manage.php
@@ -2,7 +2,7 @@
 
 return [
     
-    // Login Page...
+    // Authentication...
     "login" => [
     	'page_title' => 'ورود به سامانه' ,
         'head_title' => 'ورود به سامانه' ,
@@ -10,6 +10,59 @@ return [
         'error_password' => 'شناسه کاربری یا رمز عبور اشتباه است..' ,
     	'error_deleted'	 => 'دست‌رسی به این حساب امکان‌پذیر نیست.',
     	'error_not_published' => 'دست‌رسی به این حساب، فعلاً امکان‌پذیر نیست.',
+    ],
+    
+    "global" => [
+    	'page_title' => 'سفیران زندگی',
+    ],
+    
+    "menu" => [
+        'logout' => 'خروج',
+    	'profile'	=> 'حساب کاربری',
+    	'dashboard'	=> 'پیشخوان',
+        'index'	=> 'پیشخوان',
+    	'search-all' => 'جست‌وجوی همه چیز',
+    
+    	'cards' => 'کارت‌های اهدای عضو',
+        'cards_browse' => 'پیمایش',
+    	'cards_verify' => 'نیازمند تأیید',
+        'cards_new_one' => 'ثبت نام انفرادی',
+        'cards_new_bulk' => 'ثبت نام دسته‌جمعی',
+        'cards_print_bulk' => 'چاپ دسته‌جمعی',
+        'cards_search' => 'جست‌وجوی صاحبان کارت',
+        'cards_report' => 'گزارش‌گیری',
+        'cards_meli' => 'استعلام کارت ملی',
+
+        'volunteers' => 'سفیران زندگی',
+        'volunteers_browse' => 'پیمایش',
+        'volunteers_verify' => 'نیازمند تأیید',
+        'volunteers_search' => 'جست‌وجوی سفیر',
+        'volunteers_exams' => 'سامانه‌ی آزمون سفیران',
+        'volunteers_new' => 'ثبت نام سفیر',
+        'volunteers_report' => 'گزارش‌گیری',
+
+   		'settings'=> 'تنظیمات',
+    	'settings_profile' => 'حساب کاربری',
+        'settings_socials'  => 'شبکه‌های اجتماعی',
+        'settings_contacts' => 'راه‌های ارتباطی',
+    	'settings_general' => 'تنظیمات عمومی',
+        
+        'devSettings' => 'تنظیمات بالادستی',
+    	'devSettings_domains' => 'دامنه‌ها' ,
+    	'devSettings_states' => 'شهرها و استان‌ها',
+        'devSettings_activities'=> 'علاقه‌مندی‌های سفیران',
+        'devSettings_roles' => 'نقش‌های سفیران',
+        'devSettings_cats' => 'دسته‌بندی‌های مطالب',
+    
+    'celebs' => 'سفیران ویژه',
+    'faqs' => 'پرسش‌های متداول',
+    'angels' => 'فرشتگان ماندگار',
+    'donates' => 'کمک‌های مالی',
+    'submits' => 'یادمان',
+    
+    'posts' => 'اخبار و مطالب',
+    'galleries' => 'گالری‌ها',
+
     ],
     
 ];
---
+++ resources/views/manage/frame/use/0.blade.php
@@ -0,0 +1,4 @@
+@extends('manage.frame.home')
+
+@section('navbar-brand' , trans('manage.global.page_title') )
+@section('sidebar' , view('manage.frame.use.sidebar'))
