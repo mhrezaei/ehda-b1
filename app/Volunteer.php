eb12604f405bea6023c9f825f692acb80acef2eb
chieftaha <chieftaha@gmail.com>
2016-06-29 23:18:06 +0430

    first

--- app/Volunteer.php
+++ app/Volunteer.php
@@ -7,6 +7,8 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Crypt;
+
 
 class Volunteer extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
@@ -18,6 +20,87 @@ class Volunteer extends Model implements AuthenticatableContract, CanResetPasswo
 	public function volunteer_logins()
 	{
 		return $this->hasMany('App\Volunteer_login') ;
+	}
+
+	public function isDeveloper()
+	{
+		/*
+		| @TODO: A better definition of Developer (ex. by melli no.) would be a good idea.
+		*/
+	}
+
+	/*
+	|--------------------------------------------------------------------------
+	| Role Managements
+	|--------------------------------------------------------------------------
+	|
+	*/
+
+
+	public function getRoles()
+	{
+		return json_decode(Crypt::decrypt($this->roles),true);
+	}
+
+	public function setRoles($roles_array)
+	{
+		$this->roles = Crypt::encrypt(json_encode($roles_array));
+		$this->save();
+	}
+
+	public function attachRole($role , $permission=null)
+	{
+		$roles_array = $this->getRoles();
+
+		if(!isset($roles_array[$role]))
+			$roles_array[$role] = [] ;
+		if($permission) {
+			if(!in_array($permission,$roles_array[$role]))
+				array_push($roles_array[$role],$permission);
+		}
+
+		$this->setRoles($roles_array);
+	}
+
+	public function detachRole($role, $permission=null)
+	{
+		$roles_array = $this->getRoles();
+
+		if($permission)
+			if(($key = array_search($permission, $roles_array[$role])) !== false) {
+				unset($roles_array[$role][$key]);
+			}
+		else
+			unset($roles_array[$role]) ;
+
+		$this->setRoles($roles_array);
+
+	}
+
+	public function setDomains($domains)
+	{
+
+	}
+
+	public function getDomains($domains)
+	{
+
+	}
+
+
+	public function attachDomain($domain)
+	{
+
+	}
+
+	public function detachDomain($domain)
+	{
+
+	}
+
+	public function can($role, $permission = 'any' , $domain='any')
+	{
+
 	}
 
 }
