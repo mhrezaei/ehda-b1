1707837ff06c9ad70b96f42c87ad17c299ad4808
mhrezaei <mr.mhrezaei@gmail.com>
2016-07-29 20:49:54 +0430

    hadi

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -297,9 +297,7 @@ class ComposerStaticInitd22b1f41ad3b1e47c885a75b72357a10
     );
 
     public static $classMap = array (
-        'CreateJobsTable' => __DIR__ . '/../..' . '/database/migrations/2016_07_26_190738_create_jobs_table.php',
-        'CreatePasswordResetsTable' => __DIR__ . '/../..' . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
-        'CreateUsersTable' => __DIR__ . '/../..' . '/database/migrations/2014_10_12_000000_create_users_table.php',
+        'CreateJobsTable' => __DIR__ . '/../..' . '/database/migrations/2016_07_29_161657_create_jobs_table.php',
         'DatabaseSeeder' => __DIR__ . '/../..' . '/database/seeds/DatabaseSeeder.php',
         'File_Iterator' => __DIR__ . '/..' . '/phpunit/php-file-iterator/src/Iterator.php',
         'File_Iterator_Facade' => __DIR__ . '/..' . '/phpunit/php-file-iterator/src/Facade.php',
