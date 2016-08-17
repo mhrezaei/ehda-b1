ccab65c6f536b399c3cf8d625e29373c7e1b6529
mhrezaei <mr.mhrezaei@gmail.com>
2016-08-17 16:32:59 +0430

    hadi

--- resources/views/site/frame/footer_contact_info.blade.php
+++ resources/views/site/frame/footer_contact_info.blade.php
@@ -1,4 +1,4 @@
 <p class="address">
-    ملاصدرا، شیراز شمالی، خیابان حكیم اعظم، پلاك ۳۰، دانشگاه خاتم، طبقه ۲ انجمن اهدای عضو ایرانیان<br>(مشاهده برروی نقشه)<br>
-    ۸۹۱۷۴۱۳۵ ۲۱ ۹۸ +
+    {{ trans('site.global.ngo_address') }}<br><a href="https://www.google.com/maps/place/%D9%85%D9%88%D8%B3%D8%B3%D9%87+%D8%A2%D9%85%D9%88%D8%B2%D8%B4+%D8%B9%D8%A7%D9%84%DB%8C+%D8%AE%D8%A7%D8%AA%D9%85%E2%80%AD/@35.758935,51.4005634,17z/data=!4m6!1m3!3m2!1s0x3f8e0691657fd377:0xfd6f8c0337ff5a92!2z2YXZiNiz2LPZhyDYotmF2YjYsti0INi52KfZhNuMINiu2KfYqtmF!3m1!1s0x3f8e0691657fd377:0xfd6f8c0337ff5a92">{{ trans('site.global.ngo_address_see_on_map') }}</a> <br>
+    {{ trans('site.global.ngo_phone_number') }}
 </p>
--- resources/views/site/home/0.blade.php
+++ resources/views/site/home/0.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="container-fluid">
-        @include('site.site.home.slider')
-        @include('site.site.home.paragraph')
-        @include('site.site.home.event')
-        @include('site.site.home.slider_event_script')
+        @include('site.home.slider')
+        @include('site.home.paragraph')
+        @include('site.home.event')
+        @include('site.home.slider_event_script')
         @include('site.home.stats')
         @include('site.home.fix_background')
     </div>
