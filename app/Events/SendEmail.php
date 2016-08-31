25ff96ac3e2c8119532f83b1fca8f2452002e2ef
mhrezaei <mr.mhrezaei@gmail.com>
2016-08-31 23:04:46 +0430

    hadi

--- app/Events/SendEmail.php
+++ app/Events/SendEmail.php
@@ -24,6 +24,7 @@ class SendEmail extends Event
         $this->reciever_name = $reciever_name;
         $this->subject = $subject;
         $this->msg_body['data'] = $msg_body;
+        // salam
     }
 
     /**
