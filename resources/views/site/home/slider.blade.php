071714b71385460e0ca70b6628faee208af45208
mhrezaei <mr.mhrezaei@gmail.com>
2017-01-04 02:22:13 +0330

    اشیه

--- resources/views/site/home/slider.blade.php
+++ resources/views/site/home/slider.blade.php
@@ -3,6 +3,9 @@
     <div class="owl-carousel home-slider" dir="ltr">
         @foreach($slide_show as $slide)
             <div class="item" dir="rtl">
+                @if($slide->meta('link'))
+                    <a href="{{ $slide->meta('link') }}" target="_blank">
+                @endif
                 <img src="{{ $slide->say('featured_image') }}">
                 <div class="slide-text">
                     @if(strlen($slide->title))
@@ -12,6 +15,9 @@
                         <span>{{ $slide->meta('title_two') }}</span>
                     @endif
                 </div>
+                @if($slide->meta('link'))
+                    </a>
+                @endif
             </div>
         @endforeach
     </div>
