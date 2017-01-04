0eaadf1a83fe1c94934c1301ce6db724e2327570
mhrezaei <mr.mhrezaei@gmail.com>
2017-01-04 15:29:55 +0330

    hadi

---
+++ storage/framework/views/68976ca7896a7cf299a30ca721e2c3e6822921bd.php
@@ -0,0 +1,15 @@
+<div class="col-xs-12 col-sm-4">
+    <?php if(sizeof($ngo_news)): ?>
+        <h4><?php echo e(trans('site.global.news')); ?> <?php echo e(trans('site.know_menu.internal-ngo')); ?></h4>
+        <ul class="news list-unstyled">
+            <?php foreach($ngo_news as $one_news): ?>
+                <li>
+                    <a href="<?php echo e($one_news->say('link')); ?>"><?php echo App\Providers\AppServiceProvider::pd(($one_news->say('title_limit'))) ?></a>
+                    <span class="date"><?php echo App\Providers\AppServiceProvider::pd((jDate::forge($one_news->published_at)->format('Y F d'))) ?></span>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <hr>
+        <a href="<?php echo e(url('/archive/iran-news/internal-ngo')); ?>" class="left"><?php echo e(trans('site.global.more')); ?></a>
+    <?php endif; ?>
+</div>
--- storage/framework/views/7066581a5d5d978bf74dc5fd805b479b1216cbd1.php
+++ storage/framework/views/7066581a5d5d978bf74dc5fd805b479b1216cbd1.php
@@ -3,6 +3,9 @@
     <div class="owl-carousel home-slider" dir="ltr">
         <?php foreach($slide_show as $slide): ?>
             <div class="item" dir="rtl">
+                <?php if($slide->meta('link')): ?>
+                    <a href="<?php echo e($slide->meta('link')); ?>" target="_blank">
+                <?php endif; ?>
                 <img src="<?php echo e($slide->say('featured_image')); ?>">
                 <div class="slide-text">
                     <?php if(strlen($slide->title)): ?>
@@ -12,6 +15,9 @@
                         <span><?php echo e($slide->meta('title_two')); ?></span>
                     <?php endif; ?>
                 </div>
+                <?php if($slide->meta('link')): ?>
+                    </a>
+                <?php endif; ?>
             </div>
         <?php endforeach; ?>
     </div>
