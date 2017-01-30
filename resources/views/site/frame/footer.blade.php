e705b58a688467a462a503d2ae78ce75276edbe6
mhrezaei <mr.mhrezaei@gmail.com>
2017-01-30 11:10:03 +0330

    front template make change
    # header add iwmf.ir link
    # footer add programmer link

--- resources/views/site/frame/footer.blade.php
+++ resources/views/site/frame/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-primary">
+<footer class="bg-primary" style="padding-bottom: 40px;">
     <div class="container">
         <div class="row">
             @include('site.frame.footer_menu')
@@ -6,6 +6,9 @@
                 @include('site.frame.footer_social_network')
                 @include('site.frame.footer_contact_info')
             </div>
+        </div>
+        <div class="row" style="text-align: center; color: #FFFAAA; font-size: 12px; padding-top: 20px;">
+            <a href="https://yasnateam.com" target="_blank" style="color: #FFFAAA;">طراحی و اجرا: گروه یسنا</a>
         </div>
     </div>
 </footer>
--- storage/framework/views/80bfda34e0a1b3707a90d433bf0397c3ef8dcb5a.php
+++ storage/framework/views/80bfda34e0a1b3707a90d433bf0397c3ef8dcb5a.php
@@ -1,4 +1,4 @@
-<footer class="bg-primary">
+<footer class="bg-primary" style="padding-bottom: 40px;">
     <div class="container">
         <div class="row">
             <?php echo $__env->make('site.frame.footer_menu', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
@@ -6,6 +6,9 @@
                 <?php echo $__env->make('site.frame.footer_social_network', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
                 <?php echo $__env->make('site.frame.footer_contact_info', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
             </div>
+        </div>
+        <div class="row" style="text-align: center; color: #FFFAAA; font-size: 12px; padding-top: 20px;">
+            <a href="https://yasnateam.com" target="_blank" style="color: #FFFAAA;">طراحی و اجرا: گروه یسنا</a>
         </div>
     </div>
 </footer>
